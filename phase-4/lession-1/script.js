baf32f6bd82da9ea1e939e1ab491278cfb4eab11
harsha <sunnyharsha320@gmila.com>
2024-02-13 12:30:47 +0530

    in lession1 practice project 4 changed to some more dynamic content

--- phase-4/lession-1/script.js
+++ phase-4/lession-1/script.js
@@ -1,1 +1,16 @@
 console.log("A Text From javaScript Page");
+var roll =0581;
+var b="b";
+var c="h";
+var a = 3, b=4 ,c='hello';
+console.log(a,b,c);
+console.log("concations "+ b+roll);
+var z=2,r=4,y=6;
+alert("i am harsha")
+var s = prompt( "Enter your name" );
+// alert( "Hello, " + s + "!" );
+console.log(b+s);
+var details ={firstname:"    harsha", lastname:"vardhan"}
+document.write("Small text content from the javaScript:Firstname: "+details.firstname+" Lastname: "+details.lastname);
+console.log(details.sirname="katukojwala")
+console.log(details)
